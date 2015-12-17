d846457562921f3546fa77c81e4848f77a2e0602
Halzabschneider <nicolas.schneider1@gmx.de>
2015-12-17 19:48:51 +0100

    Ruby_2

--- Rails/Projekte/flix/app/controllers/movies_controller.rb
+++ Rails/Projekte/flix/app/controllers/movies_controller.rb
@@ -2,4 +2,10 @@ class MoviesController < ApplicationController
 	def index
 		@movies = Movie.all
 	end
+
+	def show
+		@movies = Movie.find(params[:id])
+
+	end
+
 end
--- Rails/Projekte/flix/app/helpers/application_helper.rb
+++ Rails/Projekte/flix/app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+	def format_movie_description(movie)
+		if movie.description.length > 40
+			truncate(movie.description, length: 40, separator: ' ', omission: '...') #omission: gibt an was nach der Erreichten länge stehen soll. eingabe zählt zu der maximalen anzahl dazu
+		else
+			movie.description
+		end 
+	end
+
+	def format_total_gross(movie)
+		if movie.flop?
+			content_tag(:strong, "FLOP")
+		else
+			number_to_currency(movie.total_gross)
+		end
+	end
+
+
 end
