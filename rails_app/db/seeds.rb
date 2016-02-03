df21ca96532d8d6bc06779cb26c8fe7acbad986d
halzabschneider <nicolas.schneider1@gmx.de>
2016-02-03 01:03:47 +0100

    hallo

---
+++ rails_app/db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.create(:name => "Film schauen", :image_file => "batman.jpg", :capacity => 5, :description => "Bati und seine Kumpane", :location => "Gotham City", :price => 1000, :start_at => "2017-01-01")
+
